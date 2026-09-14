7d8153cf75edfbd88bcae484495b6f9f7fc2d20e
arthexis <arthexis@gmail.com>
2026-09-14 10:23:58 -0600

    Export Celery queue source

--- src/gway_epaper/sources/__init__.py
+++ src/gway_epaper/sources/__init__.py
@@ -1,4 +1,5 @@
+from .celery_queue import CeleryQueueSource
 from .files import FileSource
 from .redis_stream import RedisStreamSource
 
-__all__ = ["FileSource", "RedisStreamSource"]
+__all__ = ["CeleryQueueSource", "FileSource", "RedisStreamSource"]
