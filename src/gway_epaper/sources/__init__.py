3952b396c9fc265373979e7318b68ebdbc1c1e26
arthexis <arthexis@gmail.com>
2026-09-14 11:59:39 -0600

    Export Kombu queue source

--- src/gway_epaper/sources/__init__.py
+++ src/gway_epaper/sources/__init__.py
@@ -1,4 +1,5 @@
 from .files import FileSource
+from .kombu_queue import KombuQueueSource
 from .redis_stream import RedisStreamSource
 
-__all__ = ["FileSource", "RedisStreamSource"]
+__all__ = ["FileSource", "KombuQueueSource", "RedisStreamSource"]
